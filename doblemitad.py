68e3e8bade06fe93a4e177b60299f30d1ef6451e
William <williambarreramuoz@gmail.com>
2017-03-07 09:18:37 -0500

    dadodos numero se muestra el doble del mayor y la mitad del menor

---
+++ doblemitad.py
@@ -0,0 +1,8 @@
+numero1=int(input('Ingrese el primer numero: '))
+numero2=int(input('Ingrese el segundo numero: '))
+if(numero1<=numero2):
+	print('El doble del numero mayor es: '+str(numero2*2))
+	print('La mitad del numero menor es: '+str(numero1/2))
+else:
+	print('El doble del numero mayor es: '+str(numero1*2))
+	print('La mitad del numero menor es: '+str(numero2/2))
