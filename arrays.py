afd2c500fc1c071482708a021ec7de0cd7e6e3c2
William <williambarreramuoz@gmail.com>
2017-03-09 08:25:53 -0500

    Codigos vistos en clase

---
+++ arrays.py
@@ -0,0 +1,28 @@
+miLista=['william','camilo','daniel','farcelona'] #Pueden ser enteros, strin, float...
+#print(miLista)
+#print(miLista[9])
+#print(miLista[5:8])
+#print(miLista[:-2])
+#miLista[1:3]=['0','a',"Hola"]
+#miLista.append('No debo llegar tarde ') #append agrega otra posicion al final
+#print(miLista)
+#for valor in miLista:
+#	print(valor) #Se recorre todo el arreglo
+#print(len(miLista)) #Funcion len da el tamaño
+#print(miLista*2) #Muestra dos veces el arreglo
+#print(miLista)
+#miLista.insert(7,'a') #agrega en la posicion que le diga
+#del miLista[2] #Elimina la posicion
+print(miLista)
+#miLista.remove(5) #Elimina el valor, el primero que encuentre en el arreglo
+#miLista.pop(1) #Elimina lo que este en la posicion dada
+#miLista.clear()  #Limpia toda la lista
+#miLista2=['a','b','c','d','e']
+#print(miLista2)
+#miLista.extend(miLista2) #Extiende lo que hay en la primera lista a o que hay en la segunda
+#miLista.sort()  #Para organizar la lista <ascendente>, tiene que estar en el mismo tipo de dato
+#miLista.reverse() #Invierte la lista
+#a=miLista.index('camilo') #Busca el valor en la lista <posicion>
+#a='jose' in miLista #pregunta si jose esta en mi lista
+a='jose' not in miLista #Pregunta si jose no esta en mi lista
+print(max(miLista)) #Saca el valor maximo
